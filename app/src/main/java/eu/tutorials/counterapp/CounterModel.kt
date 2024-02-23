ef0e679bffb31a028ef0d380c235bd12441af9b1
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-02-23 05:53:47 +0530

    Added Counter Repository

--- app/src/main/java/eu/tutorials/counterapp/CounterModel.kt
+++ app/src/main/java/eu/tutorials/counterapp/CounterModel.kt
@@ -1,2 +1,18 @@
 package eu.tutorials.counterapp
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
+class CounterRepository {
+    private var count = mutableStateOf(0)
+
+    fun getCount(): MutableState<Int> = count
+
+    fun incrementCount() {
+        count.value++
+    }
+
+    fun decrementCount() {
+        count.value--
+    }
+}
