d380e03baa44f2a2b9b8bf245c890a7455f35c0c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-02-23 05:58:49 +0530

    Implemented the mian activity using ViewModel

--- app/src/main/java/eu/tutorials/counterapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/counterapp/MainActivity.kt
@@ -3,41 +3,63 @@ package eu.tutorials.counterapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import eu.tutorials.counterapp.ui.theme.CounterAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel: CounterViewModel = viewModel()
             CounterAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    CounterScreen(viewModel)
                 }
             }
         }
     }
+
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun CounterScreen(viewModel: CounterViewModel) {
+    val count = viewModel.count
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CounterAppTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Counter: ${count.value}",
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { viewModel.increment() }) {
+            Text(text = "Increment")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { viewModel.decrement() }) {
+            Text(text = "Decrement")
+        }
     }
 }
